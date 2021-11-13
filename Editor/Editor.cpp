a5719836761ed43baca9c2eddee16ee1ebd08711
Eearslya Sleiarion <eearslya@gmail.com>
2021-11-12 21:25:50 -0800

    Add preprocessor flag for debugging

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -4,7 +4,9 @@
 using namespace Luna;
 
 int main(int argc, const char** argv) {
+#ifdef LUNA_DEBUG
 	Log::SetLevel(spdlog::level::trace);
+#endif
 
 	std::unique_ptr<Engine> engine = std::make_unique<Engine>();
 
